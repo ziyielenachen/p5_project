ade7207aecbbd102514e9ccbd6ed99a70d0b6eb8
Elena Chen <elena.chen@berkeley.edu>
2019-09-22 14:55:25 -0700

    version1

---
+++ empty-example/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(1450,865);
+  background(132, 193, 240);
+}
+
+function draw() {
+  //Draw all text boxes.
+  let purple = color(255,255,204);
+  fill(purple);
+  noStroke;
+  rect((1420/2-350),500,700,30,20);
+  rect((1420/2-350),550,700,30,20);
+  rect((1420/2-350),600,700,30,20);
+  rect((1420/2-350),650,700,30,20);
+  rect((1420/2-350),700,700,30,20);
+  rect((1420/2-350),750,700,30,20);
+
+}
